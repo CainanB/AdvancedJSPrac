d0ad730b57c6ceca4227b7b9f8bc00586660ae44
Cainan Barboza <CainanBarboza1@gmail.com>
2020-09-25 17:01:33 -0500

    node routes

--- nodeprac/my-express-server/server.js
+++ nodeprac/my-express-server/server.js
@@ -1,13 +1,28 @@
 const express = require("express");
+const bodyParser = require("body-parser")
+
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true }))
 
 app.get("/", (req, res) =>{
     res.sendFile(__dirname + "/index.html")
 })
+app.get("/bmicalculator", (req, res) =>{
+    res.sendFile(__dirname + "/bmiCalculator.html")
+})
 
 app.post("/", (req, res) =>{
-    res.send("thanks!")
+   const {num1, num2} = req.body
+   result = parseInt(num1) + parseInt(num2)
+   res.send(`The result of the calculation is ${result}`)
+})
+app.post("/bmicalculator", (req, res) =>{
+   let {weight, height} = req.body
+   weight = parseInt(weight)
+   height = parseInt(height)
+   bmi = weight / (height ** 2)
+   res.send(`Your bmi is ${bmi}`)
 })
 
 
