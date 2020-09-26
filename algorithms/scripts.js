b9ee115e72e8f83ab3a66a2bb46f995800858de4
Cainan Barboza <CainanBarboza1@gmail.com>
2020-09-26 00:55:47 -0500

    d

--- algorithms/scripts.js
+++ algorithms/scripts.js
@@ -1,4 +1,5 @@
 // Multiple Pointers
+// Find first pair of numbers whose sum is zero
 
 // Naive way
 
@@ -58,16 +59,39 @@ const numArray = [-4,-3, -2, -1, 0, 1, 2, 5]
 // Practice
 // countUniqueValues
 
-const countUniqueValues = arr => {
-    let i = 0
-   for(let j = 1; j < arr.length; j++){
-       if(arr[i] !== arr[j]){
-           i++;
-           arr[i] = arr[j]
-       }
-   }
-   return arr.splice(0,arr[i])
+// const countUniqueValues = arr => {
+//     let i = 0
+//    for(let j = 1; j < arr.length; j++){
+//        if(arr[i] !== arr[j]){
+//            i++;
+//            arr[i] = arr[j]
+//        }
+//    }
+// //    return arr.splice(0,i + 1)
+// return(i + 1)
+// }
+
+// console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]))
+
+// SLIDING Window
+// Write a function called maxSubarraySum which accepts an Array
+// of integers and a number called null. The function should calculate
+// the maximum sum of n consecutive elements in the array
+
+function maxSubarraySum(arr, num){
+    let maxSum = 0
+    let tempSum = 0
+    if(arr.length < num) return null
+    for(let i = 0; i < num; i++){
+        maxSum += arr[i]
+    }
+    tempSum = maxSum
+    for(let i = num; i < arr.length; i++){
+        tempSum = tempSum - arr[i - num] + arr[i]
+        maxSum = Math.max(maxSum, tempSum)
+    }
+    return maxSum
 }
 
-countUniqueValues([1,2,3,4,4,4,7,7,12,12,13])
+console.log(maxSubarraySum([1,2,3,4,4,4,7,7,12,12,13],3))
 
