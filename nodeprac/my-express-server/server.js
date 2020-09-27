96a6c485a52eaa3839d6be98d810554aa1397425
Cainan Barboza <CainanBarboza1@gmail.com>
2020-09-26 20:15:01 -0500

    m

--- nodeprac/my-express-server/server.js
+++ nodeprac/my-express-server/server.js
@@ -1,29 +1,45 @@
 const express = require("express");
 const bodyParser = require("body-parser")
+const https = require("https");
 
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true }))
 
 app.get("/", (req, res) =>{
-    res.sendFile(__dirname + "/index.html")
-})
-app.get("/bmicalculator", (req, res) =>{
-    res.sendFile(__dirname + "/bmiCalculator.html")
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=houston,texas&units=imperial&appid=949e47da5c633330a239053d53c767ee"
+    https.get(url, (response) =>{
+        console.log(response)
+        response.on("data", function(data){
+           const weatherData = JSON.parse(data)
+            const temp = weatherData.main.temp
+            const description = weatherData.weather[0].description
+            console.log(temp, description)
+          
+        })
+    })
+    res.sendFile(__dirname + "/weather.html")
 })
 
-app.post("/", (req, res) =>{
-   const {num1, num2} = req.body
-   result = parseInt(num1) + parseInt(num2)
-   res.send(`The result of the calculation is ${result}`)
-})
-app.post("/bmicalculator", (req, res) =>{
-   let {weight, height} = req.body
-   weight = parseInt(weight)
-   height = parseInt(height)
-   bmi = weight / (height ** 2)
-   res.send(`Your bmi is ${bmi}`)
-})
+// app.get("/", (req, res) =>{
+//     res.sendFile(__dirname + "/index.html")
+// })
+// app.get("/bmicalculator", (req, res) =>{
+//     res.sendFile(__dirname + "/bmiCalculator.html")
+// })
+
+// app.post("/", (req, res) =>{
+//    const {num1, num2} = req.body
+//    result = parseInt(num1) + parseInt(num2)
+//    res.send(`The result of the calculation is ${result}`)
+// })
+// app.post("/bmicalculator", (req, res) =>{
+//    let {weight, height} = req.body
+//    weight = parseFloat(weight)
+//    height = parseFloat(height)
+//    bmi = weight / (height ** 2)
+//    res.send(`Your bmi is ${bmi}`)
+// })
 
 
 app.listen(3000, () =>{
